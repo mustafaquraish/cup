4cedbd0271b75faaa18ed6d6636d865947670175
Mustafa Quraish <mustafaq9@gmail.com>
2022-02-18 08:34:29 -0500

    Make meta/test.sh also test to see if all examples compile without error

--- meta/test.sh
+++ meta/test.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
+# Test suite
 for f in $(ls tests | grep -v "common")
 do 
     echo "Running $f"
     ./tests/$f || exit 1
+done
+
+# Test to see if all examples compile
+for f in $(ls examples/*.cup)
+do
+    echo "Testing compilation of: $f"
+    ./build/cupcc $f -s -o build/test || exit 1
 done
